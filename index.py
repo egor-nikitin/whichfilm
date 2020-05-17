4f4ab87f8da3aa043ecb0246eae6f67b7d2895e4
Егор Никитин <egornikitin@MacBook-Pro-Egor.local>
2020-05-17 14:42:31 +0300

    Fix

--- index.py
+++ index.py
@@ -8,46 +8,55 @@ import time
 
 from flask import Flask, jsonify, Response, request
 app = Flask(__name__)
+ctx = {}
 
-config = {
-    'apiKey': 'AIzaSyA2LihYUsMhVdZa60XBSZ3j4IUaSGh0NOI',
-    'authDomain': 'whichfilmbot.firebaseapp.com',
-    'databaseURL': 'https://whichfilmbot.firebaseio.com',
-    'projectId': 'whichfilmbot',
-    'storageBucket': 'whichfilmbot.appspot.com',
-    'messagingSenderId': '914533040757',
-    'appId': '1:914533040757:web:6410e7bf9f046353cc2e3c'
-}
+def init():
+    config = {
+        'apiKey': 'AIzaSyA2LihYUsMhVdZa60XBSZ3j4IUaSGh0NOI',
+        'authDomain': 'whichfilmbot.firebaseapp.com',
+        'databaseURL': 'https://whichfilmbot.firebaseio.com',
+        'projectId': 'whichfilmbot',
+        'storageBucket': 'whichfilmbot.appspot.com',
+        'messagingSenderId': '914533040757',
+        'appId': '1:914533040757:web:6410e7bf9f046353cc2e3c'
+    }
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-db_auth_updated_at = 0
-db_user = {}
-db = firebase.database()
+    firebase = pyrebase.initialize_app(config)
 
-def refresh_db_auth():
-    if db_auth_updated_at == 0:
-        db_user = auth.sign_in_with_email_and_password('yegor.nikitin@gmail.com', 'sdHds3@dkIq8pp')
-        db_auth_updated_at = time.time()
-    elif (time.time() - db_auth_updated_at > 3500):
-        db_user = auth.refresh(db_user['refreshToken'])
-        db_auth_updated_at = time.time()
+    ctx = {
+        'auth': firebase.auth(),
+        'db_auth_updated_at': 0,
+        'db_user': {},
+        'db': firebase.database(),
+        'firebase': firebase,
+        'items': []
+    }
 
-items = []
+    return ctx
+
+def refresh_db_auth(ctx):
+    if ctx['db_auth_updated_at'] == 0:
+        ctx['db_user'] = ctx['auth'].sign_in_with_email_and_password('yegor.nikitin@gmail.com', 'sdHds3@dkIq8pp')
+        ctx['db_auth_updated_at'] = time.time()
+    elif (time.time() - ctx['db_auth_updated_at'] > 3500):
+        ctx['db_user'] = ctx['auth'].refresh(ctx['db_user']['refreshToken'])
+        ctx['db_auth_updated_at'] = time.time()
+
 more_button_text = 'еще'
 
-def update_items_cache():
-    if items == []:
-        db_items = db.child(db_user['localId']).child("items").get(db_user['idToken'])
+def update_items_cache(ctx):
+    if ctx['items'] == []:
+        db_items = ctx['db'].child(ctx['db_user']['localId']).child("items").get(ctx['db_user']['idToken'])
         for db_item in db_items:
             item = db_item.val()
             item['id'] = db_item.key()
-            items.append(item)
+            ctx['items'].append(item)
 
-def update_item_in_db(item):
-    db.child(db_user['localId']).child("items").child(item['id']).update(item, db_user['idToken'])
+def update_item_in_db(ctx, item):
+    ctx['db'].child(ctx['db_user']['localId']).child("items").child(item['id']).update(item, ctx['db_user']['idToken'])
 
-def get_recommendation(text):
+def get_recommendation(ctx, text):
+    items = ctx['items']
     items_with_tag = []
     if text and not text.startswith('/') and text != more_button_text:
         items_with_tag = [x for x in items if text in x['tags']]
@@ -87,7 +96,7 @@ def get_biggest_photo_file_id(photos):
             file_id = photo.file_id
     return file_id
 
-def send_item(bot, chat, item):
+def send_item(ctx, bot, chat, item):
     text = get_item_text(item)
     tags_plus_extra = item['tags'] + [more_button_text]
     if not 'file_id' in item and 'image_url' in item:
@@ -98,7 +107,7 @@ def send_item(bot, chat, item):
                       reply_markup=get_tags_keyboard(tags_plus_extra))
         item['photo'] = message.photo
         item['file_id'] = get_biggest_photo_file_id(message.photo)
-        update_item_in_db(item)
+        update_item_in_db(ctx, item)
     elif 'file_id' in item:
         bot.sendPhoto(chat_id=chat.id,
                       photo=item['file_id'],
@@ -111,14 +120,14 @@ def send_item(bot, chat, item):
                         text=text,
                         reply_markup=get_tags_keyboard(tags_plus_extra))
 
-def reply(bot, message):
-    item = get_recommendation(message.text)
-    send_item(bot, message.chat, item)
+def reply(ctx, bot, message):
+    item = get_recommendation(ctx, message.text)
+    send_item(ctx, bot, message.chat, item)
 
-def reply_to_inline(bot, query):
-    item = get_recommendation(query.data)
+def reply_to_inline(ctx, bot, query):
+    item = get_recommendation(ctx, query.data)
     bot.answerCallbackQuery(callback_query_id=query.id)
-    send_item(bot, query.message.chat, item)
+    send_item(ctx, bot, query.message.chat, item)
 
 @app.route('/', methods=['GET'])
 def getme():
@@ -133,16 +142,18 @@ def api():
         return jsonify({"status": "error", "reason": "no tg token"})
     
     bot = telegram.Bot(TELEGRAM_TOKEN)
+    if not ctx:
+        ctx = init()
     
     if request.method == "POST":
-        refresh_db_auth()
-        update_items_cache()
+        refresh_db_auth(ctx)
+        update_items_cache(ctx)
         update = telegram.Update.de_json(request.get_json(force=True), bot)
 
         if update.message:
-            reply(bot, update.message)
+            reply(ctx, bot, update.message)
         else:
-            reply_to_inline(bot, update.callback_query)
+            reply_to_inline(ctx, bot, update.callback_query)
     else:
         return str(bot.get_me())
         
