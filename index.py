af7c5adcb06ae417ca1c17816f7d32118d11d70c
Егор Никитин <egornikitin@MacBook-Pro-Egor.local>
2020-05-28 22:41:53 +0300

    New bot

--- index.py
+++ index.py
@@ -237,8 +237,33 @@ def getme():
     bot = telegram.Bot(TELEGRAM_TOKEN)
     return str(bot.get_me())    
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api2', methods=['GET', 'POST'])
 def api():
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN_NEW")
+    if TELEGRAM_TOKEN is None:
+        return jsonify({"status": "error", "reason": "no tg token"})
+    
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+    
+    if request.method == "POST":
+        random.seed()
+        db.refresh_auth(ctx)
+        db.update_items_cache(ctx)
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+        if update.message:
+            db.update_chats_cache(ctx, update.message.from_user, update.message.chat)
+            reply(ctx, bot, update.message)
+        else:
+            db.update_chats_cache(ctx, update.callback_query.from_user, update.callback_query.message.chat)
+            reply_to_inline(ctx, bot, update.callback_query)
+    else:
+        return str(bot.get_me())
+        
+    return jsonify({"status": "ok"})
+
+@app.route('/api', methods=['GET', 'POST'])
+def api_old():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     if TELEGRAM_TOKEN is None:
         return jsonify({"status": "error", "reason": "no tg token"})
